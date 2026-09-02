36b828b3b604d9d0031cbd94cfd09a3e710a0c76
Jayanthrx <jayanthgowdaj59@gmail.com>
2026-09-02 18:48:00 +0530

    Add unit tests and Docker Hub push step to CI/CD pipeline

---
+++ test_app.py
@@ -0,0 +1,27 @@
+import unittest
+
+from app import app
+
+
+class AppTestCase(unittest.TestCase):
+    def setUp(self):
+        self.client = app.test_client()
+
+    def test_home_page(self):
+        response = self.client.get("/")
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            response.get_data(as_text=True),
+            "Hello! My DevOps Flask Application is Running.",
+        )
+
+    def test_health_endpoint(self):
+        response = self.client.get("/health")
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.get_data(as_text=True), "Application is healthy!")
+
+
+if __name__ == "__main__":
+    unittest.main()
